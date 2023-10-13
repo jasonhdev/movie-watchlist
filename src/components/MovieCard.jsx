349bb5e66119c9759a14ea6c1e51402afe7732c2
jasonhdev <jasonhdev@gmail.com>
2023-10-13 15:32:49 -0700

    Add rating logos and scores

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,30 +8,60 @@ const MovieCard = ({ movie }) => {
             </div>
 
             <div className="infoContainer">
-                    <div className="titleRow">
-                        <a className="title" target="_blank" rel="noreferrer" href={'https://www.google.com/search?q=' + movie.title}>{movie.title}</a>
-                        {/* TODO: Add settings */}
-                    </div>
-
-                    <p className="metaDataRow">
-                        {movie.rating &&
-                            <span className="movieRating">{movie.rating}</span>
-                        }
-                        <span>{movie.year}</span>
-                        <span>{movie.runtime}</span>
-                    </p>
-
-                    <p>
-                        <i>{movie.genre}</i>
-                    </p>
-
-                    {/* TODO: Mising logic */}
-                    <p>Watch on:</p>
-
-                    {/* TODO: Expand/hide desc */}
-                    <p>{movie.description}</p>
+                <div className="titleRow">
+                    <a className="title" target="_blank" rel="noreferrer" href={'https://www.google.com/search?q=' + movie.title}>{movie.title}</a>
+                    {/* TODO: Add settings */}
                 </div>
+
+                <p className="metaDataRow">
+                    {movie.rating &&
+                        <span className="movieRating">{movie.rating}</span>
+                    }
+                    <span>{movie.year}</span>
+                    <span>{movie.runtime}</span>
+                </p>
+
+                <p>
+                    <i>{movie.genre}</i>
+                </p>
+
+                <p className="metaDataRow">
+                    {movie.rating &&
+                        <span className="movieRating">{movie.rating}</span>
+                    }
+                    <span>{movie.year}</span>
+                    <span>{movie.runtime}</span>
+                </p>
+
+                <p>
+                    <i>{movie.genre}</i>
+                </p>
+
+                {(movie.tomato || movie.imdb) &&
+                    <div className="ratingsRow">
+                        {movie.tomato &&
+                            <span>
+                                <img class="tomatoLogo" src="tomato.png"></img>
+                                <span class="ratingScore">{movie.tomato}</span>
+                            </span>
+                        }
+
+                        {movie.imdb &&
+                            <span>
+                                <img class="imdbLogo" src="imdb.png"></img>
+                                <span class="ratingScore">{movie.imdb}</span>
+                            </span>
+                        }
+                    </div>
+                }
+
+                {/* TODO: Mising logic */}
+                <p>Watch on:</p>
+
+                {/* TODO: Expand/hide desc */}
+                <p>{movie.description}</p>
             </div>
+        </div>
     );
 };
 
