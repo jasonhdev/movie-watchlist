2618eb7fd3e7d46e3c7263cab9c23c333b3da8ff
jasonhdev <jasonhdev@gmail.com>
2023-10-13 15:32:50 -0700

    Install sass, convert MovieCard css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,33 +1,21 @@
-import './MovieCard.css';
+import './MovieCard.scss';
 
 const MovieCard = ({ movie }) => {
     return (
         <div className="movieCard">
-            <div className="imgContainer">
-                <img className="movieImg" src={'https://' + movie.image} alt={'Movie poster for ' + movie.search}></img>
+            <div className="posterContainer">
+                <img src={'https://' + movie.image} alt={'Movie poster for ' + movie.search}></img>
             </div>
 
             <div className="infoContainer">
                 <div className="titleRow">
-                    <a className="title" target="_blank" rel="noreferrer" href={'https://www.google.com/search?q=' + movie.title}>{movie.title}</a>
+                    <a target="_blank" rel="noreferrer" href={'https://www.google.com/search?q=' + movie.title}>{movie.title}</a>
                     {/* TODO: Add settings */}
                 </div>
 
                 <p className="metaDataRow">
                     {movie.rating &&
-                        <span className="movieRating">{movie.rating}</span>
-                    }
-                    <span>{movie.year}</span>
-                    <span>{movie.runtime}</span>
-                </p>
-
-                <p>
-                    <i>{movie.genre}</i>
-                </p>
-
-                <p className="metaDataRow">
-                    {movie.rating &&
-                        <span className="movieRating">{movie.rating}</span>
+                        <span className="rating">{movie.rating}</span>
                     }
                     <span>{movie.year}</span>
                     <span>{movie.runtime}</span>
@@ -38,18 +26,18 @@ const MovieCard = ({ movie }) => {
                 </p>
 
                 {(movie.tomato || movie.imdb) &&
-                    <div className="ratingsRow">
+                    <div className="scoresRow">
                         {movie.tomato &&
                             <span>
                                 <img class="tomatoLogo" src="tomato.png"></img>
-                                <span class="ratingScore">{movie.tomato}</span>
+                                <span class="score">{movie.tomato}</span>
                             </span>
                         }
 
                         {movie.imdb &&
                             <span>
                                 <img class="imdbLogo" src="imdb.png"></img>
-                                <span class="ratingScore">{movie.imdb}</span>
+                                <span class="score">{movie.imdb}</span>
                             </span>
                         }
                     </div>
