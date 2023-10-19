34bf4e919855254420e266070aae111f9fd9c2f8
jasonhdev <jasonhdev@gmail.com>
2023-10-19 14:55:30 -0700

    Pass currentTab prop between header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 import './Header.scss';
 import Searchbar from './Searchbar.jsx';
 import Tabs from './Tabs';
 
 const Header = () => {
+
+    const [currentTab, setCurrentTab] = useState('watch');
+
+    const handleTabChange = (tab) => {
+        setCurrentTab(tab);
+    }
+
     return (
         <div className="header">
-            <img src="pizzachicken.png"></img>
+            <img src="pizzachicken.png" alt="Site logo"></img>
             <div className="searchBar">
-                <Searchbar></Searchbar>
+                <Searchbar currentTab={currentTab}></Searchbar>
             </div>
-            <Tabs></Tabs>
+            <Tabs handleTabChange={handleTabChange}></Tabs>
         </div>
     );
 };
--- client/src/components/Header/Tabs.jsx
+++ client/src/components/Header/Tabs.jsx
@@ -1,18 +1,29 @@
 import "./Header.scss";
+import { useState } from "react";
 
-const Tabs = () => {
+const Tabs = ({ handleTabChange }) => {
 
     let tabs = {
         'watch': 'Watch',
         'upcoming': 'Upcoming',
         'history': 'History',
-    }
+    };
+
+    const [currentTab, setCurrentTab] = useState('watch');
 
     return (
         <div className="tabs">
             {Object.entries(tabs).map(([tabKey, tabName]) => {
                 return (
-                    <button className={`tab-btn ${tabKey == 'watch' ? "active" : ""}`}>{tabName}</button>
+                    <button
+                        key={tabKey}
+                        className={`tab-btn ${tabKey === currentTab ? 'active' : ''}`}
+                        onClick={() => {
+                            setCurrentTab(tabKey);
+                            handleTabChange(tabKey);
+                        }}>
+                        {tabName}
+                    </button>
                 );
             })}
         </div>
