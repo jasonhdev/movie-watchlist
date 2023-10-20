8a955d63a45cadf54e463f17ec00d0761217572a
jasonhdev <jasonhdev@gmail.com>
2023-10-19 18:49:45 -0700

    Fix: fetch movies on page load

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,30 @@ import './App.css';
 import Movies from "./components/Movies"
 import MovieCard from "./components/MovieCard"
 import Header from "./components/Header/Header"
-import { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 
 function App() {
 
   var data = require('./data.json');
 
   const [currentTab, setCurrentTab] = useState('watch');
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState(null);
 
+  useEffect(() => {
+    const loadMovies = async () => {
+      setMovies(await getMovies());
+    }
+
+    loadMovies();
+  }, [])
 
   const handleTabChange = async (tab) => {
     setCurrentTab(tab);
-
     setMovies(await getMovies(tab));
   }
 
   const getMovies = async (list = "watch") => {
-    // fetch('http://watchapi.pizzachicken.xyz/?list=' + list)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     return data;
-    //   });
-
     const response = await fetch('http://watchapi.pizzachicken.xyz/?list=' + list)
     return await response.json();
   }
@@ -37,14 +38,16 @@ function App() {
       <Header handleTabChange={handleTabChange} currentTab={currentTab}></Header>
 
       <Movies>
-        {movies.map((movie, i) => {
-          return (
-            <MovieCard
-              key={i}
-              movie={movie}
-            />
-          );
-        })}
+        {movies !== null &&
+          movies.map((movie, i) => {
+            return (
+              <MovieCard
+                key={i}
+                movie={movie}
+              />
+            );
+          })
+        }
       </Movies>
     </div>
   );
