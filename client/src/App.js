3c6efc684d4d61915ca315cd1ecb12624f2b4596
jasonhdev <jasonhdev@gmail.com>
2023-10-26 16:45:17 -0700

    Filter movie list based on search term

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 function App() {
   const [currentTab, setCurrentTab] = useState('watch');
   const [movies, setMovies] = useState([]);
+  const [moviesCache, setMoviesCache] = useState([]);
 
   useEffect(() => {
     const loadMovies = async () => {
@@ -21,6 +22,27 @@ function App() {
     setMovies(await getMovies(tab));
   }
 
+  const handleSearchInput = (e) => {
+    let search = e.target.value;
+
+    if (!search) {
+      setMovies(moviesCache);
+      setMoviesCache([]);
+    } else {
+      if (!moviesCache.length) {
+        setMoviesCache(movies);
+        setMovies(movies.filter((movie) => {
+          return search.toLowerCase().split(' ').every(v => movie.title.toLowerCase().includes(v))
+        }));
+      }
+      else {
+        setMovies(moviesCache.filter((movie) => {
+          return search.toLowerCase().split(' ').every(v => movie.title.toLowerCase().includes(v))
+        }));
+      }
+    }
+  }
+
   const getMovies = async (list = "watch") => {
     const response = await fetch('http://watchapi.pizzachicken.xyz/?list=' + list)
     return await response.json();
@@ -31,7 +53,7 @@ function App() {
       {/* <header className="App-header">
       </header> */}
 
-      <Header handleTabChange={handleTabChange} currentTab={currentTab}></Header>
+      <Header handleTabChange={handleTabChange} handleSearchInput={handleSearchInput} currentTab={currentTab}></Header>
       <Movies movies={movies} currentTab={currentTab}></Movies>
     </div>
   );
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,13 +2,13 @@ import './Header.scss';
 import Searchbar from './Searchbar.jsx';
 import Tabs from './Tabs';
 
-const Header = ({handleTabChange, currentTab}) => {
+const Header = ({handleTabChange, handleSearchInput, currentTab}) => {
 
     return (
         <div className="header">
             <img src="pizzachicken.png" alt="Site logo"></img>
             <div className="searchBar">
-                <Searchbar currentTab={currentTab}></Searchbar>
+                <Searchbar handleSearchInput={handleSearchInput} currentTab={currentTab}></Searchbar>
             </div>
             <Tabs handleTabChange={handleTabChange} currentTab={currentTab}></Tabs>
         </div>
