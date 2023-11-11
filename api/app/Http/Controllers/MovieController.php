a08c961e89e07566562aadcb386558c4d9677dbc
jasonhdev <jasonhdev@gmail.com>
2023-11-10 19:39:51 -0800

    Rename movies model to movie. Set project index page to start at api index. Create index movie list fetching

--- api/app/Http/Controllers/MovieController.php
+++ api/app/Http/Controllers/MovieController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $list = $request->input('list');
+
+        $movies = [];
+        switch ($list) {
+            case "upcoming":
+                $movies = Movie::select('*')
+                    ->where('watched', '=', 0)
+                    ->where('released', '=', 0)
+                    ->orderBy('released_date', 'desc')
+                    ->get();
+                break;
+            case "history":
+                $movies = Movie::select('*')
+                    ->where('watched', '=', 1)
+                    ->orderBy('watched_date', 'desc')
+                    ->get();
+                break;
+            case "watch":
+                $movies = Movie::select('*')
+                    ->where('watched', '=', 0)
+                    ->where('released', '=', 1)
+                    ->orderBy('featured', 'desc')
+                    ->orderBy('amc', 'desc')
+                    ->orderBy('add_date', 'desc')
+                    ->get();
+                break;
+                // TODO: case "amc":
+            default:
+                $movies = Movie::all();
+                break;
+        }
+
+        return response()->json($movies);
+    }
 }
--- api/routes/web.php
+++ api/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'index']);
+Route::get('/movie', [MovieController::class, 'index']);
+Route::get('/movie', [MovieController::class, 'index']);
+
