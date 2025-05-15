d729b5523782acc4adc40468e9913885356a2a90
jasonhdev <jasonhdev@gmail.com>
2025-05-15 00:34:26 -0700

    Set screen resolution in attempt to prevent different results when running headless

--- scraper/movieScraper.py
+++ scraper/movieScraper.py
@@ -21,6 +21,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 service = Service(chromedriver_path)
 options = Options()
 options.add_argument("--headless") 
+options.add_argument("window-size=1920,1080")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-blink-features=AutomationControlled")
